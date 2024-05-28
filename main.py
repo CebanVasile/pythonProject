ea4fa4a2c3811647b021a6cc405d272c59be3033
CebanVasile <113338548+CebanVasile@users.noreply.github.com>
2024-05-28 10:58:16 +0200

    first actions

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-print("HH")
+        # STEP BY STEP
+
+# Workflows are defined in .github/workflows/
+# Whenever push request => runs testing script ( defines yaml script) ih workflow repo
+# The trigger can also be manual/API based/or scheduled
+# JOBS - A job = task which will be executed/shell script; can pass down data to other job - dependency on other job
+# ACTIONS - reducing the number of repeated code - AUTH, Building ENV, Git repo pull - OWN/MARKETPLACE
+# RUNNERS - Server for running workflow when triggered; 1runner = 1job; ( Enabled hosting "virtuals")
+#
 
